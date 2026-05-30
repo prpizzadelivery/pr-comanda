0fb9943d98fe60162f0a0d239158379ba9069718
pr-pizzas <petriliani26@gmail.com>
2026-05-30 19:19:03 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.PR_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyD7HwfLT_11AbvdaBmrzp3uocjEBRJ1Xvg",
+  authDomain: "pr-pizza.firebaseapp.com",
+  databaseURL: "https://pr-pizza-default-rtdb.firebaseio.com",
+  projectId: "pr-pizza",
+  storageBucket: "pr-pizza.firebasestorage.app",
+  messagingSenderId: "1069342602821",
+  appId: "1:1069342602821:web:50e6d2b19840b78f8f152c"
+};
